226b48380de110beb97219157a310eb08713de32
Jin <qjsro368@gmail.com>
2021-05-28 18:25:32 +0900

    Youtub_29m49s

--- game.js
+++ game.js
@@ -8,8 +8,33 @@ let frames = 0;
 // LOAD SPRITE IMAGE
 const sprite = new Image();
 sprite.src = "img/sprite.png";
+
+//GAME STATE
+const state = {
+    current : 0,
+    getReady : 0,
+    game : 1,
+    over : 2
+}
+
+//CONTROL THE GAME
+cvs.addEventListener("click",function(evt){
+    switch(state.current){
+        case state.getReady:
+            state.current = state.game;
+            break;
+        case state.game:
+            bird().flap();
+            break;
+        case state.over:
+            state.current = state.getReady;
+            break;
+    }
+});
+
+
 //BACKGROUND
-const bg={
+const bg = {
     sX : 0,
     sY : 0,
     w : 275,
@@ -20,13 +45,97 @@ const bg={
     draw : function(){
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+
     }
+}
+//FOREGROUND
+const fg={
+    sX : 276,
+    sY : 0,
+    w : 224,
+    h : 112,
+    x : 0,
+    y : cvs.height - 112,
+
+    draw : function(){
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+
+
+    }
+}
+//BIRD
+const bird={
+    animation : [
+        {sX : 276, sY : 112},//frame : 0에 해당
+        {sX : 276, sY : 139},//frame : 1에 해당
+        {sX : 276, sY : 164},//frame : 2에 해당
+        {sX : 276, sY : 139}//frame : 3에 해당
+    ],
+
+    x : 50,
+    y : 150,
+    w : 34,
+    h : 26,
+
+    frame : 0,
+
+    draw : function(){
+        let bird = this.animation[this.frame];
+                ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h);
+
+    },
+
+    flap : function(){
+
+    }
+}
+
+//GET READY MESSAGE
+const getReady = {
+    sX : 0,
+    sY : 228,
+    w : 173,
+    h : 152,
+    x : cvs.width/2 - 173/2,
+    y : 80,
+
+    draw : function(){
+        if(state.current == state.getReady){
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
+
+    }
+
+}
+
+//GAME OVER MESSAGE
+const gameOver= {
+    sX : 175,
+    sY : 228,
+    w : 225,
+    h : 202,
+    x : cvs.width/2 - 225/2,
+    y : 90,
+
+    draw : function(){
+        if(state.current == state.over){
+          ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
+
+    }
+
 }
 //DRAW
 function draw(){
     ctx.fillStyle = "#70c5ce";
     ctx.fillRect(0, 0, cvs.width, cvs.height);
+
     bg.draw();
+    fg.draw();
+    bird.draw();
+    getReady.draw();
+    gameOver.draw();
 }
 //UPDATE
 function update(){
@@ -38,6 +147,8 @@ function loop(){
     update();
     draw();
     frames++;
+
+    requestAnimationFrame(loop);
 }
 
 loop();
